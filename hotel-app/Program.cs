6b453a0c58f04f75277e7f7a38ebbd173b0abb10
Asan <asanaliov01@gmail.com>
2026-06-02 10:11:01 +0200

    updated tests

--- hotel-app/Program.cs
+++ hotel-app/Program.cs
@@ -1,12 +1,28 @@
+using hotel_app.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment()) {
+    app.UseMigrationsEndPoint();
+}
+else {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
@@ -24,5 +40,10 @@ app.MapControllerRoute(
         pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+app.MapRazorPages()
+    .WithStaticAssets();
 
 app.Run();
+
+// Exposed so the integration test project can use WebApplicationFactory<Program>.
+public partial class Program { }
--- hotel-app/obj/Debug/net10.0/hotel-app.AssemblyInfo.cs
+++ hotel-app/obj/Debug/net10.0/hotel-app.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("hotel-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfffea3fe834f8b5c6fb278d7945bf4c52d68d90")]
 [assembly: System.Reflection.AssemblyProductAttribute("hotel-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hotel-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
